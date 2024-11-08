650c4dde4dc297a936b237c6cf7f9f96b4d345b4
Florian-THP <triboutflorian@outlook.com>
2024-11-08 16:40:07 +0100

    push 7

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,32 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+   @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.new(params_article)  # Associe l'article à l'utilisateur actuel
+
+    if @article.save
+      redirect_to article_path(@article)  # Redirige vers l'article nouvellement créé
+    else
+      render :new  # Si la sauvegarde échoue, affiche le formulaire de création avec les erreurs
+    end
+  end
+
+  private 
+  
+  def params_article
+    params.require(:article).permit(:name, :description, :price)
+  end
+
+
+end
