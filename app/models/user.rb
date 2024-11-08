85682b6b56be4c7e6f63a24ea38f5cc8b1bb20a2
Florian-THP <triboutflorian@outlook.com>
2024-11-08 13:59:00 +0100

    modif users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Associations
-  has_many :articles, foreign_key: 'vendor_id'  # L'utilisateur peut être un vendeur d'articles
-  has_many :orders  # L'utilisateur peut faire des commandes (acheter des articles)
-  has_many :purchased_articles, through: :orders, source: :article  # Les articles achetés par l'utilisateur
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+ # Associations
+ has_many :articles, foreign_key: 'vendor_id'  # L'utilisateur peut être un vendeur d'articles
+ has_many :orders  # L'utilisateur peut faire des commandes (acheter des articles)
+ has_many :purchased_articles, through: :orders, source: :article  # Les articles achetés par l'utilisateur
+
 end
