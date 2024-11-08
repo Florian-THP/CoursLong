c57d6de0d2f80ee0a7e3d24f69946298e408aa09
Florian-THP <triboutflorian@outlook.com>
2024-11-08 15:44:51 +0100

    push 6

--- app/controllers/profil_controller.rb
+++ app/controllers/profil_controller.rb
@@ -1,14 +1,11 @@
 class ProfilController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:show]  # Sécuriser uniquement l'action show
 
-  def show 
-
+  def show
     if params[:id].to_i == current_user.id
       @user = current_user
     else 
       @user = User.find(params[:id])
     end
-
   end
-
 end
